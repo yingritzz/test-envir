9f63b06ccb954cd43cf51ff8fcf618156432dc98
PorntipS <62428405+yingritzz@users.noreply.github.com>
2020-12-14 13:26:47 +0700

    test
    integrate template
    -header
    -menu
    -footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
