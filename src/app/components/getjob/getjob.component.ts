8aa44f34b76a9fd18c71b1689c6ac6a0a010c274
WisarootK <74397696+WisarootK@users.noreply.github.com>
2020-12-17 16:54:35 +0700

    getjob

--- src/app/components/getjob/getjob.component.ts
+++ src/app/components/getjob/getjob.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetjobComponent implements OnInit {
 
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
+  add:any
+  selectedGroup: any;
+getVal() {
+    // console.log(this.selectedGroup); // returns selected object
+    console.log(this.selectedGroup.name); // returns selected option's id
+    if(this.selectedGroup == 'เพิ่มอุปกรณ์ใหม่'){
+    console.log("สวัสดี"); // returns selected option's name
+    }
+}
+myFunction() {
+  console.log("สวัสดี");
+}
 
+  groups = [{
+      "name": "CO2NO2"
+  },
+  {
+      "name": "NO2"
+  },
+  {
+      "name": "NOx"
+  }];
 }
