ec0ac42e9383ac96eb69dca1c0aa20b137cebe5a
WisarootK <74397696+WisarootK@users.noreply.github.com>
2020-12-28 11:34:36 +0700

    fix bug

--- src/app/LINE/line-maintenanc/line-maintenanc.component.ts
+++ src/app/LINE/line-maintenanc/line-maintenanc.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineMaintenancComponent implements OnInit {
 
+maintenanc: any = [["200DA200310704", "Thermo PM 2.5", "1", "ว่าง 1"], ["xxxxxxxxxxxxxx", "สาย Thermo PM 2.5", "5", "ว่าง 3"]]
   constructor() { }
 
   ngOnInit(): void {
