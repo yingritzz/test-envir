4cef7683686cca936f92dad798a6d308712d0f32
WisarootK <74397696+WisarootK@users.noreply.github.com>
2021-01-20 16:21:43 +0700

    job edit not yet

--- src/app/components/job-edit/job-edit.component.ts
+++ src/app/components/job-edit/job-edit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
 import { ActivatedRoute,Router } from '@angular/router';
 import { ApiService } from '../../services/api.service'
+import { EqDetailComponent } from '../eq-detail/eq-detail.component';
+import { EmploymentDetail } from '../../models/employment-detail';
 
 
 @Component({
@@ -13,63 +15,60 @@ export class JobEditComponent implements OnInit {
 
   statusArray: string[] = [];
   jobEdit: any;
-  type: any;
   id: any;
-  rentals: string[] = ["กำลังเช่า-ยืม","ส่งของให้ลูกค้า","ลูกค้ารับสินค้า"];
-  testing: string[] = ["กำลังทดสอบ","ส่งของให้ลูกค้า","ลูกค้ารับสินค้า"];
-  maintenanc: string[] = ["กำลังซ่อมบำรุง","ส่งของให้ลูกค้า","ลูกค้ารับสินค้า"];
-  selling: string[] = ["กำลังจำหน่าย","ส่งของให้ลูกค้า","ลูกค้ารับสินค้า"];
-  jobUpdate: any;
+  rentals: string[] = ["รับงาน","กำลังเช่า-ยืม","ส่งของให้ลูกค้า","ลูกค้ารับสินค้า"];
+  testing: string[] = ["รับงาน","กำลังทดสอบ","ส่งของให้ลูกค้า","ลูกค้ารับสินค้า"];
+  maintenanc: string[] = ["รับงาน","กำลังซ่อมบำรุง","ส่งของให้ลูกค้า","ลูกค้ารับสินค้า"];
+  selling: string[] = ["รับงาน","กำลังจำหน่าย","ส่งของให้ลูกค้า","ลูกค้ารับสินค้า"];
+  jobUpdate: EmploymentDetail;
   test: any;
-  re = /\{/gi;
-  
-  
-  
 
+  xxx: any = [];
+  xxy: any;
+  
   constructor(
     private location: Location,
     public router: Router,
     public activatedRoute: ActivatedRoute,
     public apiService: ApiService) {
       this.jobEdit = [];
+      this.jobUpdate = new EmploymentDetail();
      }
 
      ngOnInit(): void {
-      this.type = this.activatedRoute.snapshot.params["type"];
       this.id = this.activatedRoute.snapshot.params["id"];
       this.getJobEdit();
     }
   
     getJobEdit() {
-      this.apiService.getEmploymentDetail(this.type,this.id).then((res: any) => {
+      this.apiService.getEmploymentDetail(this.id).then((res: any) => {
         console.log(res);
         this.jobEdit = res;
-        this.test = res[0].toString().replace(this.re,"");
-        console.log(res[0].amount.split(","));
-        console.log(this.test);
       });
     }
     statusChange(status: any){
       if (status == "เช่า-ยืม"){
         this.statusArray = this.rentals;
-        console.log(this.statusArray);
 
       } else if (status == "ซ่อมบำรุง") {
         this.statusArray = this.maintenanc; 
-        console.log(this.statusArray);
 
       } else if (status == "ทดสอบ") {
         this.statusArray = this.testing;
-        console.log(this.statusArray);
 
       } else if (status == "จำหน่าย") {
         this.statusArray = this.selling;
-        console.log(this.statusArray);
       }
     }
 
+    trackByIndex(index: number, obj: any): any {
+      return index;
+    }
+
   onClickBack() {
-    this.location.back();
+    // this.location.back();
+    console.log(this.xxy)
   }
+
 
 }
