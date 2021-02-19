eaba6b943a50aefa82574759c0670818172d4046
PorntipS <62428405+yingritzz@users.noreply.github.com>
2021-02-19 14:55:34 +0700

    test-line

--- src/app/LINE/test/test.component.ts
+++ src/app/LINE/test/test.component.ts
@@ -26,7 +26,7 @@ export class TestsComponent implements OnInit {
         if (liff.isInClient()) {
           document.getElementById('test')?.append((await liff.getProfile()).displayName);
         }else {
-          liff.login({redirectUri: 'https://bit.ly/3uc9dZp'})
+          liff.login({redirectUri: 'https://bit.ly/3boaNyC'})
         }
 
       });
