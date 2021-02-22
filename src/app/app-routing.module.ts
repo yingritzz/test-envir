009de844dda91140bc27d09aafe3abda6bc09116
PorntipS <62428405+yingritzz@users.noreply.github.com>
2021-02-22 11:28:28 +0700

    hash-url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -64,10 +64,11 @@ const routes: Routes = [
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    scrollPositionRestoration: 'top', useHash: false
-  })],
-  // imports: [RouterModule.forRoot(routes, { useHash: true })],
+  // imports: [RouterModule.forRoot(routes, {
+  //   scrollPositionRestoration: 'top', useHash: false
+  // })],
+  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top', useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
